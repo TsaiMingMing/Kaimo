cb83278f724adbd9e05ad086317a695244899567
TsaiMingMing <d111111guy@gmail.com>
2022-08-16 16:45:22 +0800

    0816

---
+++ js/author.js
@@ -0,0 +1,7 @@
+// $(function(){
+// $("img").mouseenter(function() {
+// $("img").removeClass("sepia")
+// }).mouseleave(function(event) {
+// $("img").addClass("sepia")
+// });;
+// })
--- php/newsql.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con=new mysqli('localhost','Kaimau','25016486','kaimau');
-
-$sql = "SELECT * FROM news ";
-$result = mysqli_query($con,$sql);
-
-
